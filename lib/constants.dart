882bdec24cf08876c1af900780154c32f3ab68f5
hrimG <hrimgandhi@gmail.com>
2020-07-04 19:00:24 +0530

    dark mode UI flutter app

---
+++ lib/constants.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 75.0;
+const kActiveCardColor = Color(0xFF1D1F33);
+const kInactiveCardColor = Color(0xFF111328);
+const kBottomContainerColor = Color(0xFFEB1555);
+const kTextColor = Color(0xFF8D8E98);
+const kOverlaySliderColor = Color(0x29EB1555);
+
+const kMinHeight = 120;
+const kMaxHeight = 220;
+
+const kLabelTextStyle = TextStyle(
+                          color: kTextColor,
+                          fontSize: 18.0
+                        );
+
+const kDigitsTextStyle = TextStyle(
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w900,
+                      );
+
+const kLargeButtonTextStyle = TextStyle( 
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold
+                          );
+                    
+const kTitleTextStyle = TextStyle( 
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.bold
+                          );
+
+const kResultTextStyle = TextStyle( 
+                          fontSize: 22.0,
+                          color: Color(0xFF24D876),
+                          fontWeight: FontWeight.bold
+                          );
+
+const kBMITextStyle = TextStyle( 
+                          fontSize: 100.0,
+                          fontWeight: FontWeight.bold
+                          );
+
+const kBodyTextStyle = TextStyle( 
+                          fontSize: 22.0,
+                          );
